58834faa62710087dcee19b4b245409f139ad9c0
marcelolaba <63062775+marcelolaba@users.noreply.github.com>
2020-07-03 11:03:54 -0300

    scroll with jquery

--- js/app.js
+++ js/app.js
@@ -19,23 +19,50 @@ function myFunction() {
 
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    // prevent default
-    e.preventDefault();
-    links.classList.remove("show-links");
+// const scrollLinks = document.querySelectorAll(".scroll-link");
+// scrollLinks.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     // prevent default
+//     e.preventDefault();
+//     links.classList.remove("show-links");
 
-    const id = e.target.getAttribute("href").slice(1);
-    const element = document.getElementById(id);
-    //
-    let position = element.offsetTop - 62;
+//     const id = e.target.getAttribute("href").slice(1);
+//     const element = document.getElementById(id);
+//     //
+//     let position = element.offsetTop - 62;
 
-    window.scrollTo({
-      left: 0,
-      // top: element.offsetTop,
-      top: position,
-      behavior: "smooth",
-    });
+//     window.scrollTo({
+//       left: 0,
+//       // top: element.offsetTop,
+//       top: position,
+//       behavior: "smooth",
+//     });
+//   });
+// });
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
   });
 });
